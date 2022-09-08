ed3b6759a0d7802ebe33df63d211b267eef6d58f
codemetik <irfanovaiyalkharish03@gmail.com>
2022-09-08 23:20:55 +0700

    merubah file koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,15 @@
 <?php 
-/*$hostname = 'localhost';
+$hostname = 'localhost';
 $username = 'smkg6671';
 $password = 'smksapra2123';
-$database = 'smkg6671_pengawas'; */
+$database = 'smkg6671_pengawas';
 
+/* 
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'smkg6671_pengawas';
+$database = 'smkg6671_pengawas'; 
+*/
 
 $koneksi = mysqli_connect($hostname, $username, $password, $database)
 or die('Could not connect: ' . mysqli_connect_error());
