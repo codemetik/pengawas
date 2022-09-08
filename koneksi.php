6f8d04c701b31ecd19fd0792645a710c1a412d43
codemetik <irfanovaiyalkharish03@gmail.com>
2022-09-08 23:33:53 +0700

    cek tampilan di local computer

--- koneksi.php
+++ koneksi.php
@@ -4,12 +4,12 @@ $username = 'smkg6671';
 $password = 'smksapra2123';
 $database = 'smkg6671_pengawas';
 
-/* 
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'smkg6671_pengawas'; 
-*/
+
+// $hostname = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database = 'smkg6671_pengawas'; 
+
 
 $koneksi = mysqli_connect($hostname, $username, $password, $database)
 or die('Could not connect: ' . mysqli_connect_error());
